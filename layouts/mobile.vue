eafde54bc5c481cf91b9d86a042bd76e2a34abce
Rach840 <rach.balayan1@gmail.com>
2025-05-23 03:58:22 +0300

    add Mobile Menu

--- layouts/mobile.vue
+++ layouts/mobile.vue
@@ -1,10 +1,17 @@
 <template>
   <UApp>
-        <UContainer class="py-6">
-          <slot />
-        </UContainer>
+    <UContainer class="py-6 pb-20">
+      <slot />
+
+    </UContainer>
+<MobileMenu/>
   </UApp>
 </template>
+
 <script setup lang="ts">
 
 </script>
+
+<style scoped>
+/* Можно добавить дополнительные стили для Drawer, если нужно */
+</style>
